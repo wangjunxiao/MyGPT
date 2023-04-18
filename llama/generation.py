ccacbf9a0d2c2100fe4d9df7478e5add4c414ee0
wangjunxiao <wangjunxiao@live.com>
2023-04-18 19:54:26 +0300

    updated 4.18

---
+++ llama/generation.py
@@ -0,0 +1,131 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# This software may be used and distributed according to the terms of the GNU General Public License version 3.
+
+from typing import List
+
+import torch
+
+from llama.tokenizer import Tokenizer
+
+
+class LLaMA:
+    def __init__(self, model, tokenizer: Tokenizer):
+        self.model = model
+        self.tokenizer = tokenizer
+        
+    def _should_stop(self, tokens, prompt_tokens, stop_ids, stop_words):
+        """credits go to: https://github.com/galatolofederico/vanilla-llama"""
+        if stop_ids is not None:
+            do_stop = [False for _ in range(len(tokens))]
+            for i, (t, p) in enumerate(zip(tokens, prompt_tokens)):
+                g = t[len(p):].tolist()
+                for stop_id in stop_ids:
+                    if stop_id in g:
+                        do_stop[i] = True
+
+            if all(do_stop):
+                return True
+
+        if stop_words is not None:
+            do_stop = [False for _ in range(len(tokens))]
+            for i, (t, p) in enumerate(zip(tokens, prompt_tokens)):
+                t = t.clone()
+                g = t[len(p):]
+                g[g == self.tokenizer.pad_id] = self.tokenizer.eos_id
+                g = g.tolist()
+                d = self.tokenizer.decode(g)
+                for stop_word in stop_words:
+                    if stop_word in d:
+                        do_stop[i] = True
+
+            if all(do_stop):
+                return True
+
+        return False
+
+    def generate(
+        self,
+        prompts: List[str],
+        max_gen_len: int,
+        temperature: float = 0.8,
+        top_p: float = 0.95,
+        stop_ids: List[int] = None,
+        stop_words: List[str] = None,
+    ) -> List[str]:
+        bsz = len(prompts)
+        params = self.model.params
+        assert bsz <= params.max_batch_size, (bsz, params.max_batch_size)
+
+        prompt_tokens = [self.tokenizer.encode(x, bos=True, eos=False) for x in prompts]
+
+        min_prompt_size = min([len(t) for t in prompt_tokens])
+        max_prompt_size = max([len(t) for t in prompt_tokens])
+
+        total_len = min(params.max_seq_len, max_gen_len + max_prompt_size)
+
+        tokens = torch.full((bsz, total_len), self.tokenizer.pad_id).cuda().long()
+        for k, t in enumerate(prompt_tokens):
+            tokens[k, : len(t)] = torch.tensor(t).long()
+        input_text_mask = tokens != self.tokenizer.pad_id
+        start_pos = min_prompt_size
+        prev_pos = 0
+        for cur_pos in range(start_pos, total_len):
+            i = tokens[:, prev_pos:cur_pos]
+            logits = self.model(i, prev_pos)
+            if temperature > 0:
+                probs = torch.softmax(logits / temperature, dim=-1)
+                next_token = sample_top_p(probs, top_p)
+            else:
+                next_token = torch.argmax(logits, dim=-1)
+            next_token = next_token.reshape(-1)
+            # only replace token if prompt has already been generated
+            next_token = torch.where(
+                input_text_mask[:, cur_pos], tokens[:, cur_pos], next_token
+            )
+            tokens[:, cur_pos] = next_token
+            prev_pos = cur_pos
+            
+            if self._should_stop(tokens, prompt_tokens, stop_ids, stop_words):
+                break
+
+        tokens[tokens == self.tokenizer.pad_id] = self.tokenizer.eos_id
+        decoded = []
+        for i, t in enumerate(tokens.tolist()):
+            # cut to max gen len
+            t = t[: len(prompt_tokens[i]) + max_gen_len]
+            # cut to eos tok if any
+            try:
+                t = t[: t.index(self.tokenizer.eos_id)]
+            except ValueError:
+                pass
+            decoded.append(self.tokenizer.decode(t))
+        # print(decoded)
+        return [postprocessing(i, stop_words) for i in decoded]
+
+
+def postprocessing(output_text, stop_words=None, threshold=10):
+    sentences = output_text.split(".")
+    filtered_sentences = []
+    for sentence in sentences:
+        sentence = sentence.strip()
+        if len(sentence) > threshold and sentence[-1] == ".":
+            filtered_sentences.append(sentence)
+    r = '.'.join(sentences).strip()
+    if stop_words:
+        for w in stop_words:
+            if r.endswith(w):
+                r = r[0:-len(w)].strip()
+    if r[-1] != '.':
+        r += '...'
+    return r
+
+
+def sample_top_p(probs, p):
+    probs_sort, probs_idx = torch.sort(probs, dim=-1, descending=True)
+    probs_sum = torch.cumsum(probs_sort, dim=-1)
+    mask = probs_sum - probs_sort > p
+    probs_sort[mask] = 0.0
+    probs_sort.div_(probs_sort.sum(dim=-1, keepdim=True))
+    next_token = torch.multinomial(probs_sort, num_samples=1)
+    next_token = torch.gather(probs_idx, -1, next_token)
+    return next_token
---
+++ llama/tokenizer.py
@@ -0,0 +1,56 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# This software may be used and distributed according to the terms of the GNU General Public License version 3.
+
+from sentencepiece import SentencePieceProcessor
+from logging import getLogger
+from typing import *
+import os
+
+from transformers.tokenization_utils import PreTrainedTokenizer
+
+logger = getLogger()
+
+
+class Tokenizer:
+    def __init__(self, model_path: str):
+        # reload tokenizer
+        assert os.path.isfile(model_path), model_path
+        self.sp_model = SentencePieceProcessor(model_file=model_path)
+        # print(f"loaded SentencePiece model from {model_path}")
+
+        # BOS / EOS token IDs
+        self.n_words: int = self.sp_model.vocab_size()
+        self.bos_id: int = self.sp_model.bos_id()
+        self.eos_id: int = self.sp_model.eos_id()
+        self.pad_id: int = self.sp_model.pad_id()
+        # print(f"#words: {self.n_words} - BOS ID: {self.bos_id} - EOS ID: {self.eos_id}")
+        assert self.sp_model.vocab_size() == self.sp_model.get_piece_size()
+
+    def encode(self, s: str, bos: bool, eos: bool) -> List[int]:
+        assert type(s) is str
+        t = self.sp_model.encode(s)
+        if bos:
+            t = [self.bos_id] + t
+        if eos:
+            t = t + [self.eos_id]
+        return t
+
+    def decode(self, t: List[int]) -> str:
+        return self.sp_model.decode(t)
+
+
+if __name__ == "__main__":
+    def get_args():
+        import argparse
+
+        parser = argparse.ArgumentParser()
+        parser.add_argument(
+            "--tokenizer_path", type=str, default="../llama_model/tokenizer.model"
+        )
+        return parser.parse_args()
+
+    t = Tokenizer(model_path=get_args().tokenizer_path)
+    string_words = "hello world " * 10
+    print(string_words)
+    outputs = t.encode(string_words, False, False)
+    print(len(outputs), outputs)
